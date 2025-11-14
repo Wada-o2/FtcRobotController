fabf8f673c0f72d230edf28ffb5308ebefbd6fc7
Wada <aidanharvey64@gmail.com>
2025-11-14 16:13:29 -0500

    Maddie made some webcam code
    She is cool

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
@@ -27,15 +27,13 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+
 @TeleOp(name="Basic Linear Drive", group="Linear OpMode")
-public class BasicOpMode_Linear extends LinearOpMode {
+public class DriveTest extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
