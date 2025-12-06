c53642a34398c24a70126d31efc277e1229975d7
Wada <aidanharvey64@gmail.com>
2025-12-06 13:34:01 -0500

    Robot Move and Shoot
    Code didnt commit last time

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumFieldOrientedOpMode.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Mechanism.MechanumDrive;
+
+@TeleOp
+public class MechanumFieldOrientatedOpMode extends OpMode
+{
+    MechanumDrive drive = new MechanumDrive();
+
+    double forward, strafe, rotate;
+
+    @Override
+    public void init()
+    {
+        drive.init(hardwareMap);
+    }
+    public void loop()
+    {
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFieldRelative(forward,strafe,rotate);
+
+    }
+}
